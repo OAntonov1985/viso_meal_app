03ee57be227b5eb3d80894a964a0a1d14cb10c50
OAntonov1985 <aantonov1985@gmail.com>
2025-03-03 17:16:01 +0200

    try # 5

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    "compilerOptions": {
-        "baseUrl": "src",
-        "paths": {
-            "@store/*": ["store/*"]
-        }
-    }
-}
-    ;
+const nextConfig = {};
 
 export default nextConfig;
